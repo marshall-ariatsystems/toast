e6105edecd2ce58f55a711197072381adc059cdb
marshall-ariatsystems <148501680+marshall-ariatsystems@users.noreply.github.com>
2025-04-14 19:43:47 -0400

    Add files via upload

---
+++ RunToastHidden.cmd
@@ -0,0 +1,2 @@
+REM This file is intended to be used when the toast notification script is used with scheduled tasks with the Hidden.vbs file. See the documentation for further details
+powershell.exe -file "C:\ProgramData\Toast\New-ToastNotification.ps1"
