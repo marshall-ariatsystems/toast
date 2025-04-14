2af05fd665c6ec6a81ad14eb3f294d4e658d6522
marshall-ariatsystems <148501680+marshall-ariatsystems@users.noreply.github.com>
2025-04-14 19:56:37 -0400

    Update RunToastHidden.cmd

--- RunToastHidden.cmd
+++ RunToastHidden.cmd
@@ -1,2 +1,2 @@
 REM This file is intended to be used when the toast notification script is used with scheduled tasks with the Hidden.vbs file. See the documentation for further details
-powershell.exe -file "C:\ProgramData\Toast\New-ToastNotification.ps1"
+powershell.exe -file "C:\ProgramData\Toast\toast-main\New-ToastNotification.ps1"
